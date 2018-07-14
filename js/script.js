0c7473fdafc49566325b41eaffe010aae23985d0
Autumn <autumncbullard@gmail.com>
2018-07-14 16:35:59 -0400

    Change object to an array of objects and use the reduce method

--- js/script.js
+++ js/script.js
@@ -1,32 +1,70 @@
 function convertToRoman() {
-  const obj = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
-  };
+  const arr = [
+      {
+          symbol: 'M',
+          value: 1000
+      },
+      {
+          symbol: 'CM',
+          value: 900
+      },
+      {
+          symbol: 'D',
+          value: 500
+      },
+      {
+          symbol: 'CD',
+          value: 400
+      },
+      {
+          symbol: 'C',
+          value: 100
+      },
+      {
+          symbol: 'XC',
+          value: 90
+      },
+      {
+          symbol: 'L',
+          value: 50
+      },
+      {
+          symbol: 'XL',
+          value: 40
+      },
+      {
+          symbol: 'X',
+          value: 10
+      },
+      {
+          symbol: 'IX',
+          value: 9
+      },
+      {
+          symbol: 'V',
+          value: 5
+      },
+      {
+          symbol: 'IV',
+          value: 4
+      },
+      {
+          symbol: 'I',
+          value: 1
+      }
+  ];
+
   let num = $('#entered-num').val();
-  let result = '';
-  let i;
 
   if (!isNaN(num) && num >= 1 && num <= 3999) {
+    const result = arr.reduce((acc, key) => {
 
-    for (i in obj) {
-
-      while (num >= obj[i]) {
-        result += i;
-        num -= obj[i];
+      while (num >= key.value) {
+        acc += key.symbol;
+        num -= key.value;
       }
-    }
+      return acc;
+    }, '');
     $('output').addClass('roman-numeral').removeClass('error').html(result);
   }
   else if (!num) {
