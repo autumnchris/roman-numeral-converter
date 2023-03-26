05ac492f099d4a486d793b874a7211f003bf926a
autumnchris <autumncbullard@gmail.com>
2023-03-26 15:40:08 -0400

    Move conversion data into a data folder

---
+++ src/data/conversion-chart.js
@@ -0,0 +1,56 @@
+const conversionChart = [
+  {
+    symbol: 'M',
+    value: 1000
+  },
+  {
+    symbol: 'CM',
+    value: 900
+  },
+  {
+    symbol: 'D',
+    value: 500
+  },
+  {
+    symbol: 'CD',
+    value: 400
+  },
+  {
+    symbol: 'C',
+    value: 100
+  },
+  {
+    symbol: 'XC',
+    value: 90
+  },
+  {
+    symbol: 'L',
+    value: 50
+  },
+  {
+    symbol: 'XL',
+    value: 40
+  },
+  {
+    symbol: 'X',
+    value: 10
+  },
+  {
+    symbol: 'IX',
+    value: 9
+  },
+  {
+    symbol: 'V',
+    value: 5
+  },
+  {
+    symbol: 'IV',
+    value: 4
+  },
+  {
+    symbol: 'I',
+    value: 1
+  }
+];
+
+export default conversionChart;
--- src/modules/Converter.js
+++ src/modules/Converter.js
@@ -1,3 +1,5 @@
+import conversionChart from "../data/conversion-chart";
+
 const Converter = (() => {
 
   function renderRomanNumeral(numValue) {
@@ -27,60 +29,6 @@ const Converter = (() => {
   }
 
   function convertToRoman(numValue) {
-    const conversionChart = [
-      {
-        symbol: 'M',
-        value: 1000
-      },
-      {
-        symbol: 'CM',
-        value: 900
-      },
-      {
-        symbol: 'D',
-        value: 500
-      },
-      {
-        symbol: 'CD',
-        value: 400
-      },
-      {
-        symbol: 'C',
-        value: 100
-      },
-      {
-        symbol: 'XC',
-        value: 90
-      },
-      {
-        symbol: 'L',
-        value: 50
-      },
-      {
-        symbol: 'XL',
-        value: 40
-      },
-      {
-        symbol: 'X',
-        value: 10
-      },
-      {
-        symbol: 'IX',
-        value: 9
-      },
-      {
-        symbol: 'V',
-        value: 5
-      },
-      {
-        symbol: 'IV',
-        value: 4
-      },
-      {
-        symbol: 'I',
-        value: 1
-      }
-    ];
     let result = conversionChart.reduce((acc, key) => {
 
       while (numValue >= key.value) {
